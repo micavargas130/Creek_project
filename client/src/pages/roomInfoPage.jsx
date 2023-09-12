137330bb5e2c85365d011c9ce2c48d74dc940b28
micavargas130 <mica.vargas2000@gmail.com>
2023-09-11 23:22:01 -0300

    slider failed. Related to #1

--- client/src/pages/roomInfoPage.jsx
+++ client/src/pages/roomInfoPage.jsx
@@ -4,36 +4,26 @@ import {useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import useFetch from "../hooks/useFetch.js";
+import { useLocation } from "react-router-dom";
 
 
 
 export default function roomInfoPage(){
 
-  const {id} = useParams('');
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
   const [lodge,setLodge] = useState(null);
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
 
-  const {data, loading, error} = useFetch(`/lodges/`)
+  const {data, loading, error} = useFetch(`/lodges/${id}`)
 
 
 
 
-  useEffect(()=>{
-    if(!id){
-      return;
-    }
-  axios.get(`/lodge/${id}`).then(response => {
-    setPlace(response.data);
-  });
-  }, [id]);
+  
 
-  const photos = [
-    {
-      src: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/30/3a/b9/huilo-huilo-cabanas-del.jpg?w=700&h=-1&s=1",
-    },
-  ];
-
+ 
   const handleOpen = (i) => {
     setSlideNumber(i);
     setOpen(true);
@@ -42,64 +32,39 @@ export default function roomInfoPage(){
 
   return (
     <div>
-      <div className="hotelContainer">
+      <div className="lodgeContainer">
         {open && (
           <div className="slider">
             <div className="sliderWrapper">
-              <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+              <img src={data.photos[slideNumber].src} alt="" className="sliderImg" />
             </div>
           </div>
         )}
-        <div className="hotelWrapper">
-          <button className="bookNow">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Tower Street Apartments</h1>
-          <div className="hotelAddress">
-            <span>Elton St 125 New york</span>
-          </div>
-          <span className="hotelDistance">
-            Excellent location – 500m from center
-          </span>
-          <span className="hotelPriceHighlight">
-            Book a stay over $114 at this property and get a free airport taxi
-          </span>
-          <div className="hotelImages">
-            {photos.map((photo, i) => (
-              <div className="hotelImgWrapper" key={i}>
+        <div className="lodgeWrapper">
+          <h1 className="lodgeTitle">{data.name}</h1>
+          <div className="lodgeImages">
+            {data.photos?.map((photo, i) => (
+              <div className="lodgeImgWrapper" key={i}>
                 <img
                   onClick={() => handleOpen(i)}
-                  src={photo.src}
+                  src={photo}
                   alt=""
-                  className="hotelImg"
+                  className="lodgeImg"
                 />
               </div>
             ))}
           </div>
-          <div className="hotelDetails">
-            <div className="hotelDetailsTexts">
-              <h1 className="hotelTitle">Stay in the heart of City</h1>
-              <p className="hotelDesc">
-                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
-                Street Apartments has accommodations with air conditioning and
-                free WiFi. The units come with hardwood floors and feature a
-                fully equipped kitchenette with a microwave, a flat-screen TV,
-                and a private bathroom with shower and a hairdryer. A fridge is
-                also offered, as well as an electric tea pot and a coffee
-                machine. Popular points of interest near the apartment include
-                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
-                from Tower Street Apartments, and the property offers a paid
-                airport shuttle service.
+          <div className="lodgeDetails">
+            <div className="lodgeDetailsTexts">
+              <h1 className="lodgeMinititle">Details</h1>
+              <p className="lodgeDesc">
+                {data.description}
               </p>
             </div>
-            <div className="hotelDetailsPrice">
-              <h1>Perfect for a 9-night stay!</h1>
-              <span>
-                Located in the real heart of Krakow, this property has an
-                excellent location score of 9.8!
-              </span>
-              <h2>
-                <b>$945</b> (9 nights)
-              </h2>
+            <div className="lodgeDetailsPrice">
+              
+                <b>Price: {data.price}</b> 
+             
               <button>Reserve or Book Now!</button>
             </div>
           </div>
